6f0f644457ebd0fda6498e5f0bfb75b72d6d78a7
RaviTejaM9602 <72485137+RaviTejaM9602@users.noreply.github.com>
2020-11-24 14:09:05 +0530

    Initial Html Practise

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Hi Ravi");
+
+var array = [100, 200, 300, 400, 500, 600];
+var length = array.length;
+var arraysize = 0;
+while (arraysize <= array.length) {
+  var element = array[arraysize];
+  console.log(element);
+  arraysize++;
+}
